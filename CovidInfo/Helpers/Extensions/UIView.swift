7965215139d13ab7c3b20fb0554f45f3ad7f2d46
Marian Arsul <marian.arsul00@e-uvt.ro>
2022-01-28 19:28:00 +0200

    Navigation Bar Structure

--- CovidInfo/Helpers/Extensions/UIView.swift
+++ CovidInfo/Helpers/Extensions/UIView.swift
@@ -10,43 +10,6 @@ import UIKit
 
 extension UIView {
 
-    /*
-    @IBInspectable var shadow: Bool {
-        get {
-            return layer.shadowOpacity > 0.0
-        }
-        set {
-            if newValue == true {
-                self.addShadow()
-            }
-        }
-    }
-
-    @IBInspectable var cornerRadius: CGFloat {
-        get {
-            return self.layer.cornerRadius
-        }
-        set {
-            self.layer.cornerRadius = newValue
-
-            if shadow == false {
-                self.layer.masksToBounds = true
-            }
-        }
-    }
-
-
-    func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
-               shadowOffset: CGSize = CGSize(width: 100.0, height: 200.0),
-               shadowOpacity: Float = 0.4,
-               shadowRadius: CGFloat = 3.0) {
-        layer.shadowColor = shadowColor
-        layer.shadowOffset = shadowOffset
-        layer.shadowOpacity = shadowOpacity
-        layer.shadowRadius = shadowRadius
-    }
-    */
-    
     @IBInspectable var dropShadow: Bool {
         set{
             if newValue {
@@ -66,5 +29,10 @@ extension UIView {
         get {
             return layer.shadowOpacity > 0
         }
+    }
+    
+    
+    class func initFromNib<T: UIView>() -> T {
+            return Bundle.main.loadNibNamed(String(describing: self), owner: nil, options: nil)?[0] as! T
     }
 }
---
+++ CovidInfo/ViewModels/CustomNavigationBar.swift
@@ -0,0 +1,77 @@
+//
+//  CustomNavigationBar.swift
+//  CovidInfo
+//
+//  Created by Milovan Arsul on 28.01.2022.
+//
+
+import UIKit
+
+class CustomNavigationBar: UIView {
+    
+    @IBOutlet var customNavigationBar: UIView!
+    @IBOutlet weak var previousPage: UIButton!
+    @IBOutlet weak var currentPage: UIButton!
+    @IBOutlet weak var accountButton: UIButton!
+    
+    func setPageTitles(previousPageTitle: String, currentPageTitle: String){
+        self.previousPage.titleLabel?.text = previousPageTitle
+        self.currentPage.titleLabel?.text = currentPageTitle
+        awakeFromNib()
+    }
+    
+    
+    @IBOutlet weak var currentPageLeadingConstraint: NSLayoutConstraint!
+    
+    @IBAction func previousPageTapped(_ sender: Any) {
+        if self.currentPage.isHidden {
+            self.currentPageLeadingConstraint.constant = 10
+            self.currentPage.isHidden = false
+            
+            UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+                self.currentPage.layoutIfNeeded()
+                self.customNavigationBar.layoutIfNeeded()
+            }, completion: {(finished: Bool) in
+            })
+        }else{
+            self.currentPageLeadingConstraint.constant = -120
+            
+            UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+                self.currentPage.layoutIfNeeded()
+                self.customNavigationBar.layoutIfNeeded()
+            }, completion: {(finished: Bool) in
+                self.currentPage.isHidden = true
+            })
+        }
+    }
+    
+    @IBAction func currentPageTapped(_ sender: Any) {
+    }
+    
+    @IBAction func accountButtonTapped(_ sender: Any) {
+        
+    }
+    
+    private func initWithNib() {
+        Bundle.main.loadNibNamed("CustomNavigationBar", owner: self, options: nil)
+        customNavigationBar.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(customNavigationBar)
+        setup()
+    }
+    
+    override func awakeFromNib() {
+        initWithNib()
+    }
+    
+    private func setup(){
+        NSLayoutConstraint.activate(
+            [
+                customNavigationBar.topAnchor.constraint(equalTo: topAnchor),
+                customNavigationBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+                customNavigationBar.bottomAnchor.constraint(equalTo: bottomAnchor),
+                customNavigationBar.trailingAnchor.constraint(equalTo: trailingAnchor),
+            ]
+        )
+    }
+    
+}
